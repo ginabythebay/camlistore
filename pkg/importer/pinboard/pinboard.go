be6603244453b916c72fd34eff367c0c49f7bb37
Gina White <ginabythebay@gmail.com>
2014-08-28 18:20:42 -0700

    added package comments.  removed some boilerplate that came from dummy.go.  provided ui so  the auth token can be set

--- pkg/importer/pinboard/pinboard.go
+++ pkg/importer/pinboard/pinboard.go
@@ -14,20 +14,44 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package pinboard implements a pinboard.in importer, using https://pinboard.in/api.
+/*
+Importer for pinboard.in, using the v1 api documented here:  https://pinboard.in/api.
+
+Note that the api document seems to use 'post' and 'bookmark'
+interchangeably.  We use 'post' everywhere in this code.
+
+Posts in pinboard are mutable; they can be edited or deleted.
+
+We handle edited posts by always reimporting everything and rewriting
+any nodes.  Perhaps this would become more efficient if we would first
+compare the meta tag from pinboard to the meta tag we have stored to
+only write the node if there are changes.
+
+We don't handle deleted posts.  One possible approach for this would
+be to import everything under a new permanode, then once it is
+successful, swap the new permanode and the posts node (note: I don't
+think I really understand the data model here, so this is sort of
+gibberish).
+
+I have exchanged email with Maciej Ceglowski of pinboard, who may in
+the future provide an api that lets us query what has changed.  We
+might want to switch to that when available to make the import process
+more light-weight.
+
+*/
+
 package pinboard
 
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"strings"
 	"time"
 
-	"camlistore.org/pkg/blob"
 	"camlistore.org/pkg/context"
 	"camlistore.org/pkg/httputil"
 	"camlistore.org/pkg/importer"
@@ -39,84 +63,92 @@ func init() {
 	importer.Register("pinboard", &imp{})
 }
 
-// imp is the pinboard importer, as a demo of how to write an importer.
-//
-// It must implement the importer.Importer interface in order for
-// it to be registered (in the init above).
-type imp struct {
-	// TODO(gina) figure out what goes here.  Seems like we might not
-	// need anything.  There is account state, but that isn't supposed
-	// to go here.
-
-	// The struct or underlying type implementing an importer
-	// holds state that is global, and not per-account, so it
-	// should not be used to cache account-specific
-	// resources. Some importers (e.g. Foursquare) use this space
-	// to cache mappings from site-specific global resource URLs
-	// (e.g. category icons) to the fileref once it's been copied
-	// into Camlistore.
-
-}
-
-func (*imp) SupportsIncremental() bool {
-	// TODO(gina) talk to pinboard person, see if there is something
-	// that makes sense here.  The posts/all call can return things
-	// created since a given time, so it is tempting to think of using
-	// that for incremental calls.  But what about things that have
-	// been modified (e.g. given different tags?)
-	return false
-}
-
-func (*imp) NeedsAPIKey() bool { return false }
-
 const (
-	// TODO(gina) nuke this
-	username = "ginabythebay"
-
-	// test with 10 results per batch for now.
-	// FIXME(gina) set to something like 1000 before checking in
-
-	// TODO(gina) collect user auth token, fill it in
-	fetchUrl = "https://api.pinboard.in/v1/posts/all?auth_token=ginabythebay:e67ec74cf4a829a233ae&format=json&results=10&todt=%s"
+	fetchUrl = "https://api.pinboard.in/v1/posts/all?auth_token=%s&format=json&results=%d&todt=%s"
 
 	timeFormat = "2006-01-02T15:04:05Z"
 	// The start of time, in pinboard's schema
 	startCursor = "0001-01-01T00:00:00Z"
 
-	pauseInterval = time.Minute
+	// Yes this is long.  It is what the api document suggests
+	pauseInterval = 5 * time.Minute
 
 	// Just a wild guess at what makes sense
-	batchLimit = 100
+	batchLimit = 10000
+
+	attrAuthToken = "authToken"
 )
 
-func (*imp) IsAccountReady(acct *importer.Object) (ready bool, err error) {
-	// TODO(gina) implement this once we have the idea of accounts!
-	return true, nil
+// We expect <username>:<some id>.  Sometimes pinboard calls this an
+// auth token and sometimes they call it an api token.
+func extractUsername(authToken string) string {
+	split := strings.SplitN(authToken, ":", 2)
+	if len(split) == 2 {
+		return split[0]
+	} else {
+		return ""
+	}
 }
 
-func (*imp) SummarizeAccount(acct *importer.Object) string {
-	return username
+func getAuthToken(acct *importer.Object) string {
+	// will return "" if nothing is set up yet
+	return acct.Attr(attrAuthToken)
+}
+
+type imp struct {
+	importer.OAuth1 // for CallbackRequestAccount and CallbackURLParameters
+}
+
+func (*imp) SupportsIncremental() bool { return false }
+
+func (*imp) NeedsAPIKey() bool { return false }
+
+func (*imp) IsAccountReady(acct *importer.Object) (ready bool, err error) {
+	ready = getAuthToken(acct) != ""
+	return ready, nil
+}
+
+func (im *imp) SummarizeAccount(acct *importer.Object) string {
+	ok, err := im.IsAccountReady(acct)
+	if err != nil {
+		return "Not configured; error = " + err.Error()
+	}
+	if !ok {
+		return "Not configured"
+	}
+	return fmt.Sprintf("Pinboard account for %s", extractUsername(getAuthToken(acct)))
 }
 
 func (*imp) ServeSetup(w http.ResponseWriter, r *http.Request, ctx *importer.SetupContext) error {
-	return nil
+	return tmpl.ExecuteTemplate(w, "serveSetup", ctx)
 }
 
-// Statically declare that our importer supports the optional
-// importer.ImporterSetupHTMLer interface.
-//
-// We do this in case importer.ImporterSetupHTMLer changes, or if we
-// typo the method name below. It turns this into a compile-time
-// error. In general you should do this in Go whenever you implement
-// optional interfaces.
-//var _ importer.ImporterSetupHTMLer = (*imp)(nil)
-
-// TODO(gina) implement this
-func (im *imp) AccountSetupHTML(host *importer.Host) string {
-	return "<h1>Hello from the pinboard importer!</h1><p>I am example HTML. This importer is a demo of how to write an importer.</p>"
-}
+var tmpl = template.Must(template.New("root").Parse(`
+{{define "serveSetup"}}
+<h1>Configuring Pinboad Account</h1>
+<form method="get" action="{{.CallbackURL}}">
+  <input type="hidden" name="acct" value="{{.AccountNode.PermanodeRef}}">
+  <table border=0 cellpadding=3>
+  <tr><td align=right>API token</td><td><input name="apiToken" size=50> (You can find it <a href="https://pinboard.in/settings/password">here</a>)</td></tr>
+  <tr><td align=right></td><td><input type="submit" value="Add"></td></tr>
+  </table>
+</form>
+{{end}}
+`))
 
 func (im *imp) ServeCallback(w http.ResponseWriter, r *http.Request, ctx *importer.SetupContext) {
+	t := r.FormValue("apiToken")
+	if t == "" {
+		http.Error(w, "Expected an API Token", 400)
+		return
+	}
+	if err := ctx.AccountNode.SetAttrs(
+		attrAuthToken, t,
+	); err != nil {
+		httputil.ServeError(w, r, fmt.Errorf("Error setting attribute: %v", err))
+		return
+	}
+	http.Redirect(w, r, ctx.AccountURL(), http.StatusFound)
 }
 
 func (im *imp) Run(ctx *importer.RunContext) (err error) {
@@ -138,14 +170,6 @@ func (im *imp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	httputil.BadRequestError(w, "Unexpected path: %s", r.URL.Path)
 }
 
-func (im *imp) CallbackRequestAccount(r *http.Request) (blob.Ref, error) {
-	return blob.ParseOrZero(""), nil
-}
-
-func (im *imp) CallbackURLParameters(acctRef blob.Ref) url.Values {
-	return url.Values{}
-}
-
 type run struct {
 	*importer.RunContext
 	im *imp
@@ -158,6 +182,7 @@ type run struct {
 }
 
 func (r *run) getPostsNode() (*importer.Object, error) {
+	username := extractUsername(getAuthToken(r.AccountNode()))
 	root := r.RootNode()
 	rootTitle := fmt.Sprintf("%s's Pinboard Account", username)
 	log.Printf("root title = %q; want %q", root.Attr(nodeattr.Title), rootTitle)
@@ -186,10 +211,44 @@ type apiPost struct {
 	Tags string
 }
 
-func (r *run) importBatch(parent *importer.Object) (keepTrying bool, err error) {
+func (r *run) importPost(post *apiPost, parent *importer.Object) error {
+	postNode, err := parent.ChildPathObject(post.Hash)
+	if err != nil {
+		return err
+	}
+
+	t, err := time.Parse(timeFormat, post.Time)
+	if err != nil {
+		return err
+	}
+
+	attrs := []string{
+		"pinboard.in:hash", post.Hash,
+		nodeattr.Type, "pinboard.in:post",
+		nodeattr.DateCreated, schema.RFC3339FromTime(t),
+		nodeattr.Title, post.Description,
+		nodeattr.URL, post.Href,
+		"pinboard.in:extended", post.Extended,
+		"pinboard.in:meta", post.Meta,
+		"pinboard.in:shared", post.Shared,
+		"pinboard.in:toread", post.ToRead,
+	}
+	if err = postNode.SetAttrs(attrs...); err != nil {
+		return err
+	}
+	if err = postNode.SetAttrValues("tag", strings.Split(post.Tags, " ")); err != nil {
+		return err
+	}
+
+	// TODO(gina) look over importer/attrs.go and see what from there we should be using
+
+	return nil
+}
+
+func (r *run) importBatch(authToken string, parent *importer.Object) (keepTrying bool, err error) {
+	start := time.Now()
 	// TODO(gina) make the duration dynamic so we can increase it when there is a 429
 	sleepDuration := r.nextAfter.Sub(time.Now())
-	log.Printf("entered batch.  Now=%v, nextAfter=%v, sleepDuration=%v", time.Now(), r.nextAfter, sleepDuration)
 	// block until we either get canceled or until it is time to run
 	select {
 	case <-r.Done():
@@ -199,7 +258,7 @@ func (r *run) importBatch(parent *importer.Object) (keepTrying bool, err error) 
 		// just proceed
 	}
 	// fetch a batch and process the results
-	u := fmt.Sprintf(fetchUrl, r.nextCursor)
+	u := fmt.Sprintf(fetchUrl, authToken, batchLimit, r.nextCursor)
 	resp, err := http.Get(u)
 	if err != nil {
 		return false, err
@@ -237,36 +296,13 @@ func (r *run) importBatch(parent *importer.Object) (keepTrying bool, err error) 
 			log.Printf("Pinboard importer: interrupted")
 			return false, context.ErrCanceled
 		}
-		postNode, err := parent.ChildPathObject(post.Hash)
+		err := r.importPost(&post, parent)
 		if err != nil {
 			return false, err
 		}
-
-		t, err := time.Parse(timeFormat, post.Time)
-		if err != nil {
-			return false, err
-		}
-
-		attrs := []string{
-			"pinboard.in:hash", post.Hash,
-			nodeattr.Type, "pinboard.in:post",
-			nodeattr.DateCreated, schema.RFC3339FromTime(t),
-			nodeattr.Title, post.Description,
-			nodeattr.URL, post.Href,
-			"pinboard.in:extended", post.Extended,
-			"pinboard.in:meta", post.Meta,
-			"pinboard.in:shared", post.Shared,
-			"pinboard.in:toread", post.ToRead,
-		}
-		if err = postNode.SetAttrs(attrs...); err != nil {
-			return false, err
-		}
-		if err = postNode.SetAttrValues("tag", strings.Split(post.Tags, " ")); err != nil {
-			return false, err
-		}
-
-		// TODO(gina) look over importer/attrs.go and see what from there we should be using
 	}
+
+	log.Printf("Imported batch of %d posts in %s", postCount, time.Now().Sub(start))
 
 	r.nextCursor = postBatch[postCount-1].Time
 	r.nextAfter = time.Now().Add(pauseInterval)
@@ -274,6 +310,7 @@ func (r *run) importBatch(parent *importer.Object) (keepTrying bool, err error) 
 }
 
 func (r *run) importPosts() (*importer.Object, error) {
+	authToken := getAuthToken(r.AccountNode())
 	parent, err := r.getPostsNode()
 	if err != nil {
 		return nil, err
@@ -281,7 +318,7 @@ func (r *run) importPosts() (*importer.Object, error) {
 
 	keepTrying := true
 	for keepTrying {
-		keepTrying, err = r.importBatch(parent)
+		keepTrying, err = r.importBatch(authToken, parent)
 		if err != nil {
 			return nil, err
 		}
